6ec8b2cf4f9611e92fd259f6e3280d79ef106b9c
Patrick Hund <pahund@team.mobile.de>
2019-08-15 17:24:40 +0200

    Implemented issue #10 – endpoint for creating Evernote todo items (#11)
    Change-Id: I8dc1fa176f09384340ff7b074d530ccc6eed1740

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const { getPath } = require('./src/utils');
 require('dotenv').config({ path: getPath('.env') });
 const synchronize = require('./scripts/synchronize');
 const deleteCache = require('./scripts/delete-cache');
+const createEvernoteTodo = require('./scripts/create-evernote-todo');
 const express = require('express');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res
@@ -27,7 +30,7 @@ app.get('/sync', async (req, res) => {
   } else {
     console.log('FAILURE');
     res
-      .status(200)
+      .status(500)
       .send('Synchronization failed')
       .end();
   }
@@ -45,8 +48,50 @@ app.get('/delete-cache', async (req, res) => {
   } else {
     console.log('FAILURE');
     res
-      .status(200)
+      .status(500)
       .send('Failed to delete cache')
+      .end();
+  }
+});
+
+app.post('/create-todo', async (req, res) => {
+  console.log('CREATING TODO');
+  const {
+    body: { secret, title }
+  } = req;
+  if (!title) {
+    const msg = 'Missing title in JSON post body';
+    console.error(msg);
+    console.log('FAILURE');
+    res.status(400).send(msg);
+    return;
+  }
+  if (!secret) {
+    const msg = 'Missing secret in JSON post body';
+    console.error(msg);
+    console.log('FAILURE');
+    res.status(400).send(msg);
+    return;
+  }
+  if (secret !== process.env.CREATE_TODO_ENDPOINT_SECRET) {
+    const msg = 'Invalid secret';
+    console.error(msg);
+    console.log('FAILURE');
+    res.status(401).send(msg);
+    return;
+  }
+  const result = await createEvernoteTodo(title);
+  if (result === 0) {
+    console.log('SUCCESS');
+    res
+      .status(200)
+      .send('New todo successfully created in Evernote')
+      .end();
+  } else {
+    console.log('FAILURE');
+    res
+      .status(500)
+      .send('Creating new todo in Evernote failed')
       .end();
   }
 });
