33db915bbf1829b2cdbc68cba75eb8eb15ec746d
Patrick Hund <pahund@team.mobile.de>
2020-07-15 12:22:04 +0200

    Improved error handling for Evernote API access
    Change-Id: I7a4f3f325bdaacaebcc7150308ef146ffe7bc900

--- src/evernote/utils/withApiErrorHandling.js
+++ src/evernote/utils/withApiErrorHandling.js
@@ -13,6 +13,18 @@ module.exports = func => async (...args) => {
           if (err.identifier === 'Notebook.guid') {
             throw new WhatsGoingOnError(`Error accessing Evernote: unknown notebook with ID ${err.key}`);
           }
+          // see https://dev.evernote.com/doc/articles/revoked_expired_auth.php
+          if (err.errorCode === 9 && err.parameter === 'authenticationToken') {
+            throw new WhatsGoingOnError(`Error accessing Evernote: authentication token has expired`);
+          }
+          if (err.errorCode && err.parameter) {
+            throw new WhatsGoingOnError(
+              `Error accessing Evernote – error code ${err.errorCode}, parameter: ${err.parameter}`
+            );
+          }
+          if (err.errorCode) {
+            throw new WhatsGoingOnError(`Error accessing Evernote – error code ${err.errorCode}`);
+          }
           throw new WhatsGoingOnError(`Error accessing Evernote`);
         default:
           throw new WhatsGoingOnError(`Error accessing Evernote: ${err.message}`);
